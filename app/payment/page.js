259683ec95fed299639deae8e4bf3c83342180f5
Gitit2meBaby <thisworldofdans@gmail.com>
2024-10-20 01:41:18 +0700

    commented outpayment page

--- app/payment/page.js
+++ app/payment/page.js
@@ -1,13 +1,13 @@
-'use client';
-import { useRouter } from 'next/router';
+// 'use client';
+// import { useRouter } from 'next/router';
 
-export default function Payment() {
-  const router = useRouter();
-  const { transaction_status } = router.query; // NganLuong will send payment details
+// export default function Payment() {
+//   const router = useRouter();
+//   const { transaction_status } = router.query; // NganLuong will send payment details
 
-  if (transaction_status === '1') {
-    return <h1>Payment Successful!</h1>;
-  } else {
-    return <h1>Payment Failed!</h1>;
-  }
-}
+//   if (transaction_status === '1') {
+//     return <h1>Payment Successful!</h1>;
+//   } else {
+//     return <h1>Payment Failed!</h1>;
+//   }
+// }
